7876399b7418c24334c68f13f5c6cf635637bf6d
Bruno Nolasco <brunosocialmedia150769@gmail.com>
2023-11-24 02:21:38 -0300

    change-html

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,28 @@ checkbox.addEventListener('change', () =>
 {
     document.body.classList.toggle('light'); 
 });
+
+const login = document.getElementById('enviar');
+
+function alterarNome()
+{
+    const nomeAlterado = document.getElementById('name');
+    const user = document.getElementById('user');
+    const password = document.getElementById('password');
+
+    if (user.value !== '' && password.value !== '')
+    {
+        nomeAlterado.innerHTML = user.value; 
+    }
+    else
+    {
+        swal("Preencha os Dados Corretamente", {buttons: false,timer: 2000,});
+    }
+
+    user.value = '';
+    password.value = '';
+
+};
+
+login.addEventListener('click', alterarNome);
+
